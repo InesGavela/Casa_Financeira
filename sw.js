833b5fbbf2d5f57f2a074013fa1d40946f04db8c
InesGavela <inesc.1992@gmail.com>
2026-09-13 16:03:00 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'casa-financeira-v2';
+const CACHE_NAME = 'casa-financeira-v4';
 const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
